2bd80c0cff61868c4a2231bf2243de8d329eafed
jadeam0 <jdpaulse27@gmail.com>
2023-07-03 16:33:59 +0200

    progress

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,10 +1,19 @@
 <template>
     <footer class="pt-3 mt-4 text-body-secondary  border-top">
-      <img src="/" alt="fb" class="m-2" width="35px" height="35px">
-      <img src="/" alt="insta" class="m-2" width="35px" height="35px">
-      <img src="/" alt="twi" class="m-2" width="35px" height="35px">
-      <img src="/" alt="dis" class="m-2" width="35px" height="35px">
-      <img src="/" alt="git" class="m-2" width="35px" height="35px">
+      <a href="https://www.facebook.com/jd.paulse/"><img src="https://i.postimg.cc/5Nvgnqq7/icons8-facebook-64.png" alt="fb" class="m-2 animate__animated animate__pulse animate__infinite	infinite" width="35px" height="35px" ></a>
+      <a href="https://instagram.com/jad27eam?igshid=NGExMmI2YTkyZg=="><img src="https://i.postimg.cc/SKXJfVcj/icons8-instagram-64.png" alt="insta" class="m-2 animate__animated animate__pulse animate__infinite	infinite" width="35px" height="35px"></a>
+      <a href="https://twitter.com/JD3320?t=v0pYs2ZjccKCg8BxpYjsCQ&s=31"><img src="https://i.postimg.cc/KY4JrX9L/icons8-twitter-64.png" alt="twi" class="m-2 animate__animated animate__pulse animate__infinite	infinite" width="35px" height="35px"></a>
+      <a href="https://github.com/jadeam0"><img src="https://i.postimg.cc/Jhy5HNn5/icons8-github-64.png" alt="git" class="m-2 animate__animated animate__pulse animate__infinite	infinite" width="35px" height="35px"></a>
       <p class="text-center">&copy; 2023 JP</p>
   </footer>
 </template>
+
+<style scoped>
+footer {
+  background-color: #e0c3fc;
+  bottom: 0;
+  left: 0;
+  height: 90px;
+  width: 100%;
+}
+</style>
